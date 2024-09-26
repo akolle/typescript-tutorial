0719b9dd7203d18023d1fb4bee04b6bf08dbc26b
akolle <andykoller55@yahoo.com>
2024-09-25 20:08:57 -0400

    simple intro to typescript assigning variables and types

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,1 +1,24 @@
-console.log('typescript tutorial')
+let awesomeName: string = 'shakeAndBake'
+awesomeName = 'something'
+awesomeName = awesomeName.toUpperCase()
+// console.log(awesomeName)
+
+// awesomeName = 20
+
+let amount = 20
+amount = 21
+amount = 21 - 6
+// amount = 'pants'
+
+let newString = 'andy'
+newString = newString.concat('and emily')
+
+let newNum = 21
+newNum = newNum + 20
+
+let newBool = true
+newBool = 1 > 2
+
+// newNum = 'andy'
+
+console.log(newBool, newNum, newString)
