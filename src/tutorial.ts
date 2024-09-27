e785e5e371e9a9eeb927a046aa6ea52832770d98
akolle <andykoller55@yahoo.com>
2024-09-27 16:56:20 -0400

    arrays and objects

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -48,3 +48,42 @@ let fruit: string[] = ['apple', 'orange']
 
 // Be careful with this one
 // let randomValues: [] = [1, 2, 4]
+
+let array: (string | boolean)[] = ['apple', true, 'orange', false]
+
+let temperatures: number[] = [20, 25, 30]
+// templates.push('hello')
+
+let colors: string[] = ['red', 'green', 'blue']
+// colors.push(true)
+
+let mixedArray: (number | string)[] = [1, 2, 'three']
+
+// OBJECTS
+let car: { brand: string; year: number } = { brand: 'toyota', year: 2020 }
+car.brand = 'ford'
+// car.brand = 2024
+
+let car1: { brand: string; year: number } = { brand: 'audi', year: 2023 }
+
+let book = { title: 'book', cost: 20 }
+let pen = { title: 'pen', cost: 10 }
+let notebook = { title: 'notebook' }
+
+// optional type in objects
+let items: { readonly title: string; cost?: number }[] = [book, pen, notebook]
+// items[0].title = 'new book'
+items[0].cost = 12
+console.log(items[2])
+
+// OBJECTS CHALLENGE
+let bike: { brand: string; year: number } = { brand: 'yamaha', year: 2010 }
+let laptop: { brand: string; year: number } = { brand: 'Dell', year: 2020 }
+
+let product1 = { title: 'shirt', price: 20 }
+let product2 = { title: 'pants' }
+
+let products: { title: string; price?: number }[] = [product1, product2]
+
+// if you do choose to have the optional price - it must follow correct type or it will error
+// products.push({ title: 'shoes', price: 'expensive' })
